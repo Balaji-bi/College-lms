95993b982dd39b3331a1d7a2d4b55d6579237b51
Balaji K <142649523+Balaji-bi@users.noreply.github.com>
2025-04-19 15:02:41 +0530

    changes in home blade file

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -88,10 +88,7 @@
 <body>
     <div class="sidebar">
         <a href="#">Assignment Generation</a>
-        <a href="#">Paper Creation</a>
-        <a href="#">Resume Generation</a>
-        <a href="#">Image Generation</a>
-        <a href="#">Rewriting Content</a>
+       
     </div>
 
     <div class="main">
